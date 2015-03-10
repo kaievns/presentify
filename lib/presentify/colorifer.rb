cef70ae9c72f207f8248d731461beaf0f5adf986
Nikolay Nemshilov <nemshilov@gmail.com>
2015-03-10 17:00:04 +1100

    Better colorifier for the code

--- lib/presentify/colorifer.rb
+++ lib/presentify/colorifer.rb
@@ -1,20 +1,23 @@
 class Presentify
   class Colorifer
-    RED     = 1
-    GREEN   = 2
-    YELLOW  = 3
-    BLUE    = 4
-    MAGENTA = 5
-    CYAN    = 6
-    WHITE   = 7
+    RED     = 31
+    GREEN   = 32
+    YELLOW  = 33
+    BLUE    = 34
+    MAGENTA = 35
+    CYAN    = 36
+    WHITE   = 37
+    GRAY    = 2
 
     CONSTANTS_RE        = /true|false|nil/
     SINGLE_STRINGS_RE   = /('|").*?((?![\\])\1)/
     MUTLTI_STRINGS_RE   = /%(Q|w)[\{\[]([\s\S]*?)[\]\}]/m
-    KEYWORDS_RE         = /while|do|end|sleep|puts|print/
+    COMMENTS_RE         = /# [^\n|\Z]+/
+    KEYWORDS_RE         = /(\s|\n|\A)(while|do|end|sleep|puts|print|if|unless|else|def|class|ensure|rescue|yield)/
     METHOD_CALLS_RE     = /\.[a-z_]+/
     ESCAPE_CODES_RE     = /\\(r|b|n|(e\[[\d\;]+(m|A)))/
     INTERPOLATIONS_RE   = /\#\{.+?\}/
+    CLASSES_RE          = /[A-Z][a-zA-Z_]+/
 
     def colorify(code)
       #code = code.gsub NUMBERS_RE, "\e[3#{RED}m\\2\e[0m\\3"
@@ -25,8 +28,10 @@ class Presentify
         INTERPOLATIONS_RE   => YELLOW,
         MUTLTI_STRINGS_RE   => GREEN,
         SINGLE_STRINGS_RE   => GREEN,
+        COMMENTS_RE         => GRAY,
         KEYWORDS_RE         => MAGENTA,
-        METHOD_CALLS_RE     => CYAN
+        METHOD_CALLS_RE     => CYAN,
+        CLASSES_RE          => YELLOW
       }
     end
 
@@ -40,8 +45,8 @@ class Presentify
         while code.match(re)
           match = $&.dup
           code.sub! match, placeholder(stash.size)
-          replacement = "\e[3#{color}m#{match}\e[0m"
-          replacement << "\e[3#{GREEN}m" if re == ESCAPE_CODES_RE || re == INTERPOLATIONS_RE
+          replacement = "\e[#{color}m#{match}\e[0m"
+          replacement << "\e[#{GREEN}m" if re == ESCAPE_CODES_RE || re == INTERPOLATIONS_RE
           stash << replacement
         end
       end
